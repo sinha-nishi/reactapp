dc32951b0d011f17dc8751d3ea2c5c8a14cabfe9
Prashant Sinha <7372627+pkvsinha@users.noreply.github.com>
2025-09-04 14:41:19 +0530

    fix: config structure and ts errors

--- packages/app/src/BrowserApplication.tsx
+++ packages/app/src/BrowserApplication.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { createRoot } from "react-dom/client";
 import { ReactApplication } from "./ReactApplication";
 import { registerSW } from "./sw/register";
-import type { AppSetup } from '@/types/Setup';
+import type { ApplicationConfiguration } from './types/ApplicationConfiguration';
 
-export function render(rootId: string, app: Partial<AppSetup>, strictValidation?: boolean) {
+export function render(rootId: string, app: Partial<ApplicationConfiguration>, strictValidation?: boolean) {
   const rootElement = document.getElementById(rootId);
   if (rootElement) {
     const root = createRoot(rootElement);
