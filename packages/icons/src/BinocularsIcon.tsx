976c1caef1974edb4ada7236b676804fc44ca7f4
sinha-nishi <sinhanishi355@gmail.com>
2026-01-19 21:34:20 +0530

    new icons

---
+++ packages/icons/src/BinocularsIcon.tsx
@@ -0,0 +1,34 @@
+// src/icons/FileIcon.tsx
+import * as React from "react";
+
+// src/icons/Icon.types.ts
+export type IconProps = {
+  size?: number | string;
+  color?: string;
+  strokeWidth?: number;
+  variant?: "outlined" | "filled";
+  className?: string;
+};
+
+export function BinocularsIcon({
+  size = 24,
+  color = "#4A90E2",
+  strokeWidth = 2,
+  variant = "outlined",
+  className,
+}: IconProps) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      fill={variant === "filled" ? color : "none"}
+      stroke={variant === "outlined" ? color : "none"}
+      strokeWidth={strokeWidth}
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 640 640"
+    >
+      <path d="M128 32l32 0c17.7 0 32 14.3 32 32l0 32-96 0 0-32c0-17.7 14.3-32 32-32zm64 96l0 320c0 17.7-14.3 32-32 32L32 480c-17.7 0-32-14.3-32-32l0-59.1c0-34.6 9.4-68.6 27.2-98.3 13.7-22.8 22.5-48.2 25.8-74.6L60.5 156c2-16 15.6-28 31.8-28l99.8 0zm227.8 0c16.1 0 29.8 12 31.8 28L459 216c3.3 26.4 12.1 51.8 25.8 74.6 17.8 29.7 27.2 63.7 27.2 98.3l0 59.1c0 17.7-14.3 32-32 32l-128 0c-17.7 0-32-14.3-32-32l0-320 99.8 0zM320 64c0-17.7 14.3-32 32-32l32 0c17.7 0 32 14.3 32 32l0 32-96 0 0-32zm-32 64l0 160-64 0 0-160 64 0z" />
+    </svg>
+  );
+}
